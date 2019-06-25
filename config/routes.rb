06b1643ace5cae413108acc8f7820f53b97e6c8e
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-24 23:02:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #need to know which record for the edit so use dynamic route and id
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 end
